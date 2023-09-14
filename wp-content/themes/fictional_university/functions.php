cbe94051214b129d9452975ff79a831c24138a6f
Theologos Mavropoulos <rusinner88@gmail.com>
2023-09-14 19:57:10 +0300

    Added meta tags for responsiveness.

--- wp-content/themes/fictional_university/functions.php
+++ wp-content/themes/fictional_university/functions.php
@@ -16,4 +16,11 @@ function university_files()
     ));
 }
 
+function university_features()
+{
+    add_theme_support('title-tag');
+}
+
 add_action('wp_enqueue_scripts', 'university_files');
+
+add_action('after_setup_theme', 'university_features');
