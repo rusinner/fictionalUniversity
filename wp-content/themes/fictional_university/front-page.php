fe7580e5ac9889658178d7b1aabb95e9dcd74e3a
Theologos Mavropoulos <rusinner88@gmail.com>
2023-09-16 16:16:51 +0300

    Added custom field type.

--- wp-content/themes/fictional_university/front-page.php
+++ wp-content/themes/fictional_university/front-page.php
@@ -20,19 +20,34 @@ get_header(); ?>
             $homepageEvents = new WP_Query(
                 array(
                     'posts_per_page' => 2,
-                    'post_type' => 'event'
+                    'post_type' => 'event',
+                    'meta_key' => 'event_date',
+                    'orderby' => 'meta_value',
+                    'order' => 'ASC'
                 )
             );
             while ($homepageEvents->have_posts()) {
                 $homepageEvents->the_post(); ?>
                 <div class="event-summary">
                     <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                        <span class="event-summary__month"><?php the_time('M'); ?></span>
-                        <span class="event-summary__day"><?php the_time('d'); ?></span>
+                        <span class="event-summary__month"><?php
+                                                            $event_date = new DateTime(get_field('event_date'));
+                                                            echo $event_date->format('M');
+                                                            ?></span>
+                        <span class="event-summary__day"><?php
+                                                            $event_date = new DateTime(get_field('event_date'));
+                                                            echo $event_date->format('d');
+                                                            ?></span>
                     </a>
                     <div class="event-summary__content">
                         <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                        <p><?php echo wp_trim_words(get_the_content(), 6); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
+                        <p><?php if (has_excerpt()) {
+                                echo get_the_excerpt();
+                            } else {
+                                echo wp_trim_words(get_the_content(), 6);
+                            } ?>
+                            <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a>
+                        </p>
                     </div>
                 </div>
             <?php }
@@ -42,7 +57,7 @@ get_header(); ?>
 
 
 
-            <p class="t-center no-margin"><a href="#" class="btn btn--blue">View All Events</a></p>
+            <p class="t-center no-margin"><a href="<?php echo get_post_type_archive_link('event'); ?>" class="btn btn--blue">View All Events</a></p>
         </div>
     </div>
     <div class="full-width-split__two">
@@ -62,7 +77,11 @@ get_header(); ?>
                     </a>
                     <div class="event-summary__content">
                         <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                        <p><?php echo wp_trim_words(get_the_content(), 6); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+                        <p><?php if (has_excerpt()) {
+                                echo get_the_excerpt();
+                            } else {
+                                echo wp_trim_words(get_the_content(), 6);
+                            }; ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
                     </div>
                 </div>
             <?php }
