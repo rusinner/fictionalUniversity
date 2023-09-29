3840a318b2913262dac024b2432607c36e33a4c8
Theologos Mavropoulos <rusinner88@gmail.com>
2023-09-29 17:12:35 +0300

    Added campuses-programs relation.

--- wp-content/themes/fictional_university/single-program.php
+++ wp-content/themes/fictional_university/single-program.php
@@ -45,7 +45,7 @@ while (have_posts()) {
                     </a>
                 </li>
 
-        <?php }
+            <?php }
             echo '</ul>';
         }
 
@@ -80,11 +80,26 @@ while (have_posts()) {
             echo '<h2 class="headline headline--medium">Upcoming ' . get_the_title() . ' Events</h2>';
             while ($homepageEvents->have_posts()) {
                 $homepageEvents->the_post();
-                get_template_part('template-parts/content', 'event');
+                get_template_part('template-parts/content-event');
             }
         }
+        wp_reset_postdata();
+        $relatedCampuses = get_field('related_campus');
 
-        ?>
+        if ($relatedCampuses) {
+            echo '<hr class="section-break" / >';
+            echo '<h2 class="headline headline--medium">' . get_the_title() . ' is Available At These Campuses:</h2>';
+
+
+            echo '<ul class="min-list link-list">';
+            foreach ($relatedCampuses as $campus) {
+            ?> <li><a href="<?php echo get_the_permalink($campus) ?>"><?php echo get_the_title($campus) ?></a> </li> <?php
+                                                                                                                    }
+                                                                                                                    echo '</ul>';
+                                                                                                                }
+
+
+                                                                                                                        ?>
     </div>
 <?php
 }
