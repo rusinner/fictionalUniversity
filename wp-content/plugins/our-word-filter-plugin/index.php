1e0f7846d7fe56a87bc2b582a04ef5effb71592f
Theologos Mavropoulos <rusinner88@gmail.com>
2023-10-27 18:44:26 +0300

    Added repalce word final functionality and options subpage.

--- wp-content/plugins/our-word-filter-plugin/index.php
+++ wp-content/plugins/our-word-filter-plugin/index.php
@@ -15,6 +15,32 @@ class OurWordFilterPlugin
     function __construct()
     {
         add_action('admin_menu', array($this, 'ourMenu'));
+
+        add_action('admin_init', array($this, 'ourSettings'));
+        //this hook actually adds plugin replace words logic
+        if (get_option('plugin_words_to_filter')) {
+            add_filter('the_content', array($this, 'filterLogic'));
+        }
+    }
+
+    function ourSettings()
+    {
+        add_settings_section('replacement-text-section', null, null, 'word-filter-options');
+        register_setting('replacementFields', 'replacementText');
+        add_settings_field('replacement-text', 'Filtered Text', array($this, 'replacementFieldHTML'), 'word-filter-options', 'replacement-text-section');
+    }
+
+    function replacementFieldHTML()
+    { ?>
+        <input type="text" name="replacementText" value="<?php echo esc_attr(get_option('replacementText', '***')); ?>">
+        <p class="description">Leave blank to simply remove the filtered words.</p>
+        <?php }
+
+    function filterLogic($content)
+    {
+        $badWords = explode(',', get_option('plugin_words_to_filter'));
+        $badWordsTrimmed = array_map('trim', $badWords);
+        return str_ireplace($badWordsTrimmed, esc_html(get_option('replacementText', "****")), $content);
     }
 
     function ourMenu()
@@ -77,7 +103,17 @@ class OurWordFilterPlugin
 
     function optionsSubPage()
     { ?>
-        Hello from the options page
+        <div class="wrap">
+            <h1>Word Filter Options</h1>
+            <form action="options.php" method="POST">
+                <?php
+                settings_errors();
+                settings_fields('replacementFields');
+                do_settings_sections('word-filter-options');
+                submit_button();
+                ?>
+            </form>
+        </div>
 <?php }
 }
 
