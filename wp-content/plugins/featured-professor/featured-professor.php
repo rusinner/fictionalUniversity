8f752ad8771a3f7b8ceeca8fef2eb70412bfefae
Theologos Mavropoulos <rusinner88@gmail.com>
2023-11-01 16:37:37 +0200

    Fetched professor data from wordpress REST API and linked them to editor frontend .

--- wp-content/plugins/featured-professor/featured-professor.php
+++ wp-content/plugins/featured-professor/featured-professor.php
@@ -3,18 +3,21 @@
 /*
   Plugin Name: Featured Professor Block Type
   Version: 1.0
-  Author: Your Name Here
+  Author: Rusinner
   Author URI: https://www.udemy.com/user/bradschiff/
 */
 
-if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
-class FeaturedProfessor {
-  function __construct() {
+class FeaturedProfessor
+{
+  function __construct()
+  {
     add_action('init', [$this, 'onInit']);
   }
 
-  function onInit() {
+  function onInit()
+  {
     wp_register_script('featuredProfessorScript', plugin_dir_url(__FILE__) . 'build/index.js', array('wp-blocks', 'wp-i18n', 'wp-editor'));
     wp_register_style('featuredProfessorStyle', plugin_dir_url(__FILE__) . 'build/index.css');
 
@@ -25,10 +28,10 @@ class FeaturedProfessor {
     ));
   }
 
-  function renderCallback($attributes) {
+  function renderCallback($attributes)
+  {
     return '<p>We will replace this content soon.</p>';
   }
-
 }
 
 $featuredProfessor = new FeaturedProfessor();
--- wp-content/plugins/featured-professor/src/index.js
+++ wp-content/plugins/featured-professor/src/index.js
@@ -1,25 +1,52 @@
-import "./index.scss"
+import "./index.scss";
+import { useSelect } from "@wordpress/data";
 
 wp.blocks.registerBlockType("ourplugin/featured-professor", {
   title: "Professor Callout",
-  description: "Include a short description and link to a professor of your choice",
+  description:
+    "Include a short description and link to a professor of your choice",
   icon: "welcome-learn-more",
   category: "common",
+  attributes: {
+    profId: { type: "string" },
+  },
   edit: EditComponent,
   save: function () {
-    return null
-  }
-})
+    return null;
+  },
+});
 
-function EditComponent() {
+function EditComponent(props) {
+  //fecth data from wordpress REST API
+  const allProfs = useSelect((select) => {
+    return select("core").getEntityRecords("postType", "professor", {
+      per_page: -1,
+    });
+  });
+
+  if (allProfs == undefined) {
+    return <p>Loading...</p>;
+  }
   return (
     <div className="featured-professor-wrapper">
       <div className="professor-select-container">
-        We will have a select dropdown form element here.
+        <select
+          onChange={(e) => props.setAttributes({ profId: e.target.value })}
+        >
+          <option value="">Select a professor</option>
+          {allProfs.map((prof) => {
+            return (
+              <option
+                value={prof.id}
+                selected={props.attributes.profId == prof.id}
+              >
+                {prof.title.rendered}
+              </option>
+            );
+          })}
+        </select>
       </div>
-      <div>
-        The HTML preview of the selected professor will appear here.
-      </div>
+      <div>The HTML preview of the selected professor will appear here.</div>
     </div>
-  )
+  );
 }
